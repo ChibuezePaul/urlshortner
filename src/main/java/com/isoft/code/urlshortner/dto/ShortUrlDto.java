b298d0c2135ec201b5b2a2a7ca142f462025daed
Chibueze.Onyekwere <Chibueze.Onyekwere@interswitchng.com>
2023-02-06 00:34:02 +0100

    url shortner completed

--- src/main/java/com/isoft/code/urlshortner/dto/ShortUrlDto.java
+++ src/main/java/com/isoft/code/urlshortner/dto/ShortUrlDto.java
@@ -3,10 +3,11 @@ package com.isoft.code.urlshortner.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 @Getter
-@Service
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ShortUrlDto {
